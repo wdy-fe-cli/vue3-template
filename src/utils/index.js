d43b015c6a8956ab1bc4bae1d5c1ddbe6fcdf014
码路芽子 <mlyz.wdy@qq.com>
2023-02-03 15:22:17 +0800

    【fix】暂存

--- src/utils/index.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import bus from './bus'
-import Storage from './storage'
-import { isEmpty } from './validate'
-
-Vue.use(Storage)
-Vue.use(bus)
-
-Vue.prototype.$isEmpty = isEmpty
