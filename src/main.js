4b1b72aee65b7fe88c7a932740e12b222e933f6f
码路芽子 <mlyz.wdy@qq.com>
2023-02-03 14:44:50 +0800

    【fix】修改 vuex

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
 import { createApp } from 'vue'
 import router from './router'
 import '@/router/filter.js'
-import store from '@/store'
-
-import './assets/css/reset.scss'
+import { createPinia } from 'pinia'
+import './assets/styles/reset.scss'
 import 'normalize.css'
 import App from './App.vue'
 import { http } from './api/index'
 
 import ElementPlus from 'element-plus'
-import './assets/css/element.scss'
+import './assets/styles/element.scss'
 
 const app = createApp(App)
+const pinia = createPinia()
 
+app.use(pinia)
 app.use(router)
-app.use(store)
 app.use(ElementPlus)
 app.mount('#app')
 
